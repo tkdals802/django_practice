8988c2ef7f6e3f842c439e7d9bb816f77ab4baba
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-03-16 19:11:51 +0900

    240316 19:12

--- common/views.py
+++ common/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from common.forms import UserForm
 
 # Create your views here.
+
+''' 
+def signup(request):
+    """
+
+    회원가입
+    """
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_date.get('password1')
+            user = authenticate(username=username, password=raw_password) #회원가입 후 자동 로그인
+            login(request, user)
+            return redirect('index')
+        else:#GET 요청일 경우 common/signup.html 반환
+            form = UserForm()
+        return render(request, 'common/signup.html', {'form':form})
+'''
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
+            return redirect('index')
+    else:
+        form = UserForm()
+    return render(request, 'common/signup.html', {'form': form})
--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/',include('common.urls')),
+    path('', views.index, name='index'),
 ]
