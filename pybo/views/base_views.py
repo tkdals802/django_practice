b06e064a230f63162179a4d69b3df2c25f1f72d4
cha-imag <100341870+tkdals802@users.noreply.github.com>
2024-03-21 19:12:33 +0900

    pybo log set

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -2,9 +2,11 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
+import logging
+logger = logging.getLogger('pybo')
 
 def index(request):
-    3/0
+    logger.info("INFO 레벨로 출력")
     """
 
     pybo 목록 출력
